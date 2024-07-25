13a2583fc251bf76c552f3534cb0cad42a133e58
Moonjonghoo <kafmjh12@gmail.com>
2024-07-25 21:13:23 +0900

    환경변경

--- src/app/components/getTodos.tsx
+++ src/app/components/getTodos.tsx
@@ -1,5 +1,3 @@
-// import { deleteTodo } from "@/lib/actions";
-import Todo from "@/lib/Model";
 import React from "react";
 
 export const dynamic = "force-dynamic";
@@ -20,11 +18,6 @@ export default async function GetTodosUI() {
         <div key={todo._id}>
           <h3>{todo.todo as string}</h3>
           <p>{todo.todoDeadline as number}</p>
-
-          {/* <form action={deleteTodo}>
-                <input hidden type="text" name="id" defaultValue={todo._id.toString()} />
-                <button className="border rounded p-2 bg-red-400">delete</button>
-              </form> */}
         </div>
       ))}
     </div>
