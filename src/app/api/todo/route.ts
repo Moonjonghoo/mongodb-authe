b716d02d48c8cd84c1fe5a6928f61b38c6829879
Moonjonghoo <kafmjh12@gmail.com>
2024-07-25 16:37:20 +0900

    add2

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -4,8 +4,5 @@ import Todo from "@/lib/Model";
 export async function GET(req: Request, { params }: { params: { id: string } }) {
   connectDB();
   const todos = await Todo.find();
-  if (todos.length === 0) {
-    return null;
-  }
   return Response.json(todos);
 }
